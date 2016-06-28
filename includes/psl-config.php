8800168fb18048f64a3590ae853c32422e14781b
Max Bender <maxbbender@gmail.com>
2016-06-28 16:51:24 -0700

    Vagrant update

--- includes/psl-config.php
+++ includes/psl-config.php
@@ -4,8 +4,8 @@
  */
 define("HOST", "localhost"); //Host to connect to
 define("USER", "root"); //The database username
-define("PASSWORD", "s?GuDvEJnz=2,q"); //Database Password
-define("DATABASE", "viral_education"); //Database
+define("PASSWORD", "domislove"); //Database Password
+define("DATABASE", "viraleducation"); //Database
 
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
