806c86affb1b5c635b70469ddf9d1bfc50ad0b92
Max Bender <maxbbender@gmail.com>
2016-06-29 17:05:48 -0700

    update for database name

--- includes/psl-config.php
+++ includes/psl-config.php
@@ -5,7 +5,7 @@
 define("HOST", "localhost"); //Host to connect to
 define("USER", "root"); //The database username
 define("PASSWORD", "domislove"); //Database Password
-define("DATABASE", "viraleducation"); //Database
+define("DATABASE", "viral_education"); //Database
 
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
