c3ecc1417ef0ccaf1d854c748a9dd849bae260d2
Max Bender <maxbbender@gmail.com>
2016-09-02 16:24:21 -0700

    Fixes 2 in #44

--- includes/functions.php
+++ includes/functions.php
@@ -726,3 +726,24 @@ $query = '
 		echo "#Query";
 	}
 }
+
+function isAMemberofClass($userID, $classID, $mysqli) {
+	$query = '
+		SELECT id
+		FROM class_members
+		WHERE class_id = ? AND user_id ?
+			';
+	if ($stmt = $mysqli->prepare($query)){
+		$stmt->bind_param("ii", $classID, $userID);
+		$stmt->execute();
+		$stmt->bind_result($username, $fname, $lname);
+		$stmt->store_result();
+		if ($stmt->num_rows > 0){
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		echo "SQL Error : " . $stmt->error;
+	}
+}
